41e9982a5e5b3d5ffa6d4e80f99e214b17afb364
Sublustrum007 <Sublustrum007@yandex.ru>
2024-04-25 19:45:42 +0300

    Initial commit

---
+++ src/model/User.java
@@ -0,0 +1,9 @@
+package model.repository;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String surname;
+    private int phone;
+    private char gender;
+}
